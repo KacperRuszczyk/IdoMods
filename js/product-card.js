2dd0f8e723153c274708f85c5a59d235f6868dfd
KacperRuszczyk <kacper.ruszczyk96@gmail.com>
2025-05-23 16:35:26 +0200

    fake component

---
+++ js/product-card.js
@@ -0,0 +1,21 @@
+﻿class ProductCard extends HTMLElement {
+  connectedCallback() {
+    const image = this.getAttribute('image');
+    const title = this.getAttribute('title');
+    const price = this.getAttribute('price');
+
+    this.innerHTML = `
+      <div class="productContainer">
+        <div class="productImageContainer">
+          <img src="${image}" alt="${title}">
+        </div>
+        <div class="productDescriptionContainer">
+          <h3>${title}</h3>
+          <p>${price}</p>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define('product-card', ProductCard);
