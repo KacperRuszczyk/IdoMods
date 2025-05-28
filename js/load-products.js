01826a57ca56218316d0da4d0611da05d6b0bd3d
KacperRuszczyk <kacper.ruszczyk96@gmail.com>
2025-05-28 15:16:33 +0200

    product mapping

---
+++ js/load-products.js
@@ -0,0 +1,26 @@
+﻿
+async function loadProducts(page = 1, size = 5) {
+  try {
+    const res = await fetch(`https://brandstestowy.smallhost.pl/api/random?pageNumber=${page}&pageSize=${size}`);
+    const json = await res.json();
+
+    const productList = document.querySelector('.productList');
+
+
+    productList.innerHTML = '';
+
+    json.data.forEach(item => {
+      const card = document.createElement('product-card');
+      card.setAttribute('image', item.image);
+      card.setAttribute('title', item.text);
+      card.setAttribute('price', '€300,00 EUR');
+      productList.appendChild(card);
+    });
+  } catch (error) {
+    console.error('Failed to load products:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadProducts();
+});
